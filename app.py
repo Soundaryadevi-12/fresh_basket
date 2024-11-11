c553853f4bf7cc3f011581546d110a65a2bc5125
Soundaryadevi-12 <166585763+Soundaryadevi-12@users.noreply.github.com>
2024-11-11 16:23:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -204,4 +204,4 @@ def admin_dashboard():
     return render_template('admin_dashboard.html', orders=orders)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
